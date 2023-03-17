e4ca33cb67c44449cd58dd829ac666a10e07ba88
agathe marie <agathe.maillol@gmail.com>
2023-03-17 17:28:36 +0000

    imported {useState} and created event to update searchQuery value

--- src/SearchEngine/SearchEngine.tsx
+++ src/SearchEngine/SearchEngine.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
+
 interface SearchEngineProps {
     query: string
-  }
+}
 
-export default function SearchEngine({query}: SearchEngineProps) {
+export default function SearchEngine({ query }: SearchEngineProps) {
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+      
+        setSearchQuery(event.target.value);
+        console.log(event.target.value);
+    }
+
     return (
         <form>
-            <input type="text" placeholder="search for..." />
+            <input type="text" placeholder="search for..." onChange={handleInput} />
             <input type="submit" value="go!" />
         </form>
     )
