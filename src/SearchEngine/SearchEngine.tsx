5bd7afef8f384d173eb054d30a056492b8bb9d33
agathe marie <agathe.maillol@gmail.com>
2023-03-17 17:37:06 +0000

    onClick, handleSubmit creates alert (next step will request api data)

--- src/SearchEngine/SearchEngine.tsx
+++ src/SearchEngine/SearchEngine.tsx
@@ -9,15 +9,22 @@ export default function SearchEngine({ query }: SearchEngineProps) {
     const [searchQuery, setSearchQuery] = useState<string>("");
 
     function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
-      
+        event.preventDefault();
         setSearchQuery(event.target.value);
         console.log(event.target.value);
+    }
+
+    function handleSubmit(event: React.MouseEvent<HTMLInputElement>) {
+        event.preventDefault();
+        alert("query submitted!");
+
     }
 
     return (
         <form>
             <input type="text" placeholder="search for..." onChange={handleInput} />
-            <input type="submit" value="go!" />
+            <input type="submit" value="go!" onClick={handleSubmit} />
+            <p>{searchQuery}</p>
         </form>
     )
 }
