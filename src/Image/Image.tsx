fb997f02868cc378a412e722ed5e76734c75e1bb
agathe marie <agathe.maillol@gmail.com>
2023-03-19 23:25:39 +0000

    using a div w/ image as background insteag of a <img>, to control size better

--- src/Image/Image.tsx
+++ src/Image/Image.tsx
@@ -1,14 +1,18 @@
+import "./Image.css";
+
 interface ImageProps {
     source: string;
     index: number
 }
 
 
-export default function Image ({source, index}: ImageProps){
+export default function Image({ source, index }: ImageProps) {
 
-return(
-    <div key={index}>
-        <img src={source} alt="#"/>
-    </div>
-)
+    return (
+        <div className="container">
+            <div className="image" style={{ backgroundImage: `url(${source})` }} key={index}>
+                {/* <img src={source} alt="#"/> */}
+            </div>
+        </div>
+    )
 }
