f8ac93137d558d1a1d14d4492e603efe5901acb3
agathe marie <agathe.maillol@gmail.com>
2023-03-20 15:59:59 +0000

    Created and exported Footer component

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <>
+            <p>All photos from <a href="https://pixabay.com" target={"_blank"}>https://pixabay.com</a></p>
+        </>
+    )
+}
