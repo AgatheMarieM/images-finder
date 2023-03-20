52a376cb75d8f52eeb18089de3f42dbf672387ad
agathe marie <agathe.maillol@gmail.com>
2023-03-20 13:18:57 +0000

    declaring const [page, setPage] locally to manage seeMore button and calls locally

--- src/SearchEngine/SearchEngine.tsx
+++ src/SearchEngine/SearchEngine.tsx
@@ -4,12 +4,12 @@ import axios, { AxiosResponse } from "axios";
 interface SearchEngineProps {
     results: any[];
     setResults: any;
-    page: number;
-    setPage: any;
+
 }
 
-export default function SearchEngine({ results, setResults, page, setPage }: SearchEngineProps) {
+export default function SearchEngine({ results, setResults }: SearchEngineProps) {
     const [query, setQuery] = useState<string>("");
+    const [page, setPage] = useState(1);
 
     const apiKey = `34572071-131273e105e5eb7248557f286`;
     const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&order=popular&page=${page}`;
@@ -32,7 +32,7 @@ export default function SearchEngine({ results, setResults, page, setPage }: Sea
 
     function incrementPage(event: React.MouseEvent<HTMLElement>) {
         event.preventDefault();
-        setPage(page+1);
+        setPage(page + 1);
         console.log(page);
         axios.get(apiUrl).then(handleResponse);
     }
