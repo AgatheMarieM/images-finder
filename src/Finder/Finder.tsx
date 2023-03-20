375ada9e22098d24b6802b7e01b1f1a05491a8f0
agathe marie <agathe.maillol@gmail.com>
2023-03-20 12:15:38 +0000

    passing props 'searchResults' and 'setSearchResults' to SearchEngine component + erased const [query, setSearchQuery], not needed here

--- src/Finder/Finder.tsx
+++ src/Finder/Finder.tsx
@@ -3,20 +3,16 @@ import SearchEngine from "../SearchEngine/SearchEngine";
 import Images from "../Images/Images";
 import { useState } from "react";
 
-/*
-interface FinderProps {
-    searchResults: string[],
-   searchQuery: string
-}*/
+
+
 
 export default function Finder() {
-    const [searchResults, setSearchResults] = useState<string[]>(['https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg', 'https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/16:9/w_2560%2Cc_limit/1521-WIRED-Cat.jpeg']);
-    const [searchQuery, setSearchQuery] = useState<string>("");
+    const [searchResults, setSearchResults] = useState([]);
 
     return (
         <>
             <Header />
-            <SearchEngine query={searchQuery} />
+            <SearchEngine results={searchResults} setResults={setSearchResults} />
             <Images results={searchResults} />
         </>
     )
