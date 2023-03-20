6f86be75e4e6b796e76f8083e00b92cfa4cc7cf5
agathe marie <agathe.maillol@gmail.com>
2023-03-20 13:16:02 +0000

    created a See More button (in progress) that calls next page from pixabay JSON

--- src/SearchEngine/SearchEngine.tsx
+++ src/SearchEngine/SearchEngine.tsx
@@ -4,14 +4,14 @@ import axios, { AxiosResponse } from "axios";
 interface SearchEngineProps {
     results: any[];
     setResults: any;
-
+    page: number;
+    setPage: any;
 }
 
-export default function SearchEngine({ results, setResults }: SearchEngineProps) {
+export default function SearchEngine({ results, setResults, page, setPage }: SearchEngineProps) {
     const [query, setQuery] = useState<string>("");
 
     const apiKey = `34572071-131273e105e5eb7248557f286`;
-    let page = 1;
     const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&order=popular&page=${page}`;
 
 
@@ -28,17 +28,29 @@ export default function SearchEngine({ results, setResults }: SearchEngineProps)
     function handleSubmit(event: React.MouseEvent<HTMLInputElement>) {
         event.preventDefault();
         axios.get(apiUrl).then(handleResponse);
+    }
 
+    function incrementPage(event: React.MouseEvent<HTMLElement>) {
+        event.preventDefault();
+        setPage(page+1);
+        console.log(page);
+        axios.get(apiUrl).then(handleResponse);
     }
 
     return (
-        <form>
-            <input type="text" placeholder="search for..." onChange={handleInput} />
-            <input type="submit" value="go!" onClick={handleSubmit} />
-            {
-                results.length > 0 && <h3>Results for "{query}"</h3>
+        <>
+            <form>
+                <input type="text" placeholder="search for..." onChange={handleInput} />
+                <input type="submit" value="go!" onClick={handleSubmit} />
+                {
+                    results.length > 0 && <h3>Results for "{query}"</h3>
 
-            }
-        </form>
+                }
+            </form>
+            <div className="SeeMoreButton">
+                <button className="button" onClick={incrementPage}>See more</button>
+            </div>
+        </>
+
     )
 }
