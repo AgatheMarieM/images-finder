37ede29c4a7c08165a60520e11d4a97239ed4465
agathe marie <agathe.maillol@gmail.com>
2023-03-20 13:47:33 +0000

    imported SeeMore component, passing it props

--- src/Finder/Finder.tsx
+++ src/Finder/Finder.tsx
@@ -1,19 +1,31 @@
 import Header from "../Header/Header";
 import SearchEngine from "../SearchEngine/SearchEngine";
 import Images from "../Images/Images";
+import SeeMore from "../SeeMore/SeeMore";
 import { useState } from "react";
-
-
-
 
 export default function Finder() {
     const [searchResults, setSearchResults] = useState([]);
+    const [searchPage, setSearchPage] = useState(1);
+    const [searchQuery, setSearchQuery] = useState<string>("");
 
     return (
         <>
             <Header />
-            <SearchEngine results={searchResults} setResults={setSearchResults} />
-            <Images results={searchResults} />
+            <SearchEngine
+                results={searchResults}
+                setResults={setSearchResults}
+                query={searchQuery}
+                setQuery={setSearchQuery}
+                page={searchPage} />
+            <Images
+                results={searchResults} />
+            <SeeMore
+                results={searchResults}
+                setResults={setSearchResults}
+                query={searchQuery}
+                page={searchPage}
+                setPage={setSearchPage} />
         </>
     )
 }
