8fec33f2d08f080a86b218776dd19fa0b504c83a
agathe marie <agathe.maillol@gmail.com>
2023-03-20 16:30:49 +0000

    conditional rendering of error message is done in SearchEngine component

--- src/Images/Images.tsx
+++ src/Images/Images.tsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import Image from "../Image/Image";
 import "./Images.css";
 
 interface ImagesProps {
     results: any[];
+    index: number;
 }
 
 export default function Images({ results }: ImagesProps) {
-
-    if (results) {
-        return (
-            <div className="images">
-                {results.map((result) => {
-                    return (
-                        <Image source={result.webformatURL} />
-                    )
-                })}
-            </div>
-        )
-    } else {
-        return <p>'No results available'</p>;
-    }
+    return (
+        <div className="images">
+            {results.map((result, index) => {
+                return (
+                    <Image index={index} source={result.webformatURL} />
+                )
+            })}
+        </div>
+    )
 }
