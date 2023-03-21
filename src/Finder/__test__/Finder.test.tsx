371140d42c0b92ced88498fedc1535b9f598a1e9
agathe marie <agathe.maillol@gmail.com>
2023-03-21 17:48:25 +0000

    Tested if images are rendered or not (depending on results array null or not)

--- src/Finder/__test__/Finder.test.tsx
+++ src/Finder/__test__/Finder.test.tsx
@@ -45,5 +45,52 @@ describe('Finder', () => {
             expect(seeMoreButtonAfterSearch).not.toBeInTheDocument();
         })
     })
+
+    //Test 3: Confirm if Image component is shown whenever searchResults.length>0
+    it('Image component rendered', async () => {
+
+        const searchResults = ["a", "b", "c"];
+        const searchQuery = "whatever";
+
+        render(<Finder
+            searchResults={searchResults}
+            setSearchResults={mockedFunction}
+            searchPage={1}
+            setSearchPage={mockedFunction}
+            searchQuery={searchQuery}
+            setSearchQuery={mockedFunction}
+        />);
+
+        await (() => {
+            const image = screen.findByTestId("image");
+            //asynchronous wait for image to appear before continuing with the test
+            //'awaiting' for findByTestId method to finish executing
+            expect(image).toBeInTheDocument();
+        })
+    })
+
+    //Test 4: Confirm if Image component is NOT shown whenever !searchResults.length
+    it('Image component not rendered', async () => {
+
+        const searchResults = [];
+        const searchQuery = "whdgsrt";
+
+        render(<Finder
+            searchResults={searchResults}
+            setSearchResults={mockedFunction}
+            searchPage={1}
+            setSearchPage={mockedFunction}
+            searchQuery={searchQuery}
+            setSearchQuery={mockedFunction}
+        />);
+
+
+        const image = await screen.queryByTestId("image");
+        expect(image).not.toBeInTheDocument();
+        //queryByTestID returns null if element not present! getByTestID and findByTEstID throws error, not what we want
+ 
+    })
+
+
 })
 
