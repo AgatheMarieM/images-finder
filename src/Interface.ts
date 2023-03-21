4b898b88fa3c9844975f2208c808acbbd32d6eeb
agathe marie <agathe.maillol@gmail.com>
2023-03-21 12:08:53 +0000

    different interfaces for each component

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,13 +1,24 @@
-export interface FinderProps {
+export interface SearchEngineProps {
+    page: number;
+    query: string;
+    setQuery: any;
+    setResults: any;
+}
+
+export interface ImagesProps {
+    results: any[];
+    index: number;
+}
+
+export interface ImageProps {
+    source: string;
+}
+
+export interface SeeMoreProps {
     results: any[];
     setResults: any;
     page: number;
     setPage: any;
     query: string;
-    setQuery: any;
-    source: string;
-    index: number;
-
-
 }
 
