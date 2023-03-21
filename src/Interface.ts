de34bedd5e2eb68de239af2ba01cd7f9507d493d
agathe marie <agathe.maillol@gmail.com>
2023-03-21 11:56:55 +0000

    Created Interface file containing all the types needed in the app

---
+++ src/Interface.ts
@@ -0,0 +1,13 @@
+export interface FinderProps {
+    results: any[];
+    setResults: any;
+    page: number;
+    setPage: any;
+    query: string;
+    setQuery: any;
+    source: string;
+    index: number;
+
+
+}
+
