ba95b959ba1fe16555476f61d665542498a99584
agathe marie <agathe.maillol@gmail.com>
2023-03-21 09:43:14 +0000

    imported Footer.css, centered Footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import "./Footer.css";
+
 export default function Footer() {
     return (
-        <>
-            <p>All photos from <a href="https://pixabay.com" target={"_blank"}>https://pixabay.com</a></p>
-        </>
+        <div className="Footer">
+            <p >All photos from <a href="https://pixabay.com" target={"_blank"}>https://pixabay.com</a></p>
+        </div>
     )
 }
