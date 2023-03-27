3dfd4fade97ef5962ae80b3a2b88ea5ef788b0d2
agathe marie <agathe.maillol@gmail.com>
2023-03-27 15:38:15 +0100

    added toggle function to toggle heart icon

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,23 +1,28 @@
 import "./Image.css";
+import { useState } from "react";
 import { ImageProps } from "../../Interface";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as faSolidHeart} from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
 
 
 
 
 export default function Image({ source }: ImageProps) {
+    const [heartIcon, setHeartIcon] = useState(faRegularHeart);
+    const [toggle, setToggle] = useState(true);
+
+    function toggleIcon() {
+        setToggle(!toggle);
+        (heartIcon == faRegularHeart) ? setHeartIcon(faSolidHeart) : setHeartIcon(faRegularHeart);
+    }
 
     return (
         <div className="container">
             <div className="image" data-testid="image" style={{ backgroundImage: `url(${source})` }}>
                 <a className="subtitle" href={source} target="_blank">© Pixabay</a>
-            </div>           
-        
-            <FontAwesomeIcon icon={faRegularHeart} />
-            <FontAwesomeIcon icon={faSolidHeart} />
-        
+            </div>
+            <FontAwesomeIcon onClick={toggleIcon} icon={heartIcon} />
         </div>
     )
 }
