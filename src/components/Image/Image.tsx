7433a48becd28431a7ed27c9925e6dde322cc5e2
agathe marie <agathe.maillol@gmail.com>
2023-03-27 16:18:43 +0100

    bookmark icon positioned absolute inside image div

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -2,7 +2,7 @@ import "./Image.css";
 import { useState } from "react";
 import { ImageProps } from "../../Interface";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark as faBookmarkSolid  } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkSolid } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons';
 
 
@@ -14,15 +14,15 @@ export default function Image({ source }: ImageProps) {
 
     function toggleIcon() {
         setToggle(!toggle);
-        setHeartIcon(bookmarkIcon === faBookmarkRegular ? faBookmarkSolid :faBookmarkRegular);
+        setHeartIcon(bookmarkIcon === faBookmarkRegular ? faBookmarkSolid : faBookmarkRegular);
     }
 
     return (
         <div className="container">
             <div className="image" data-testid="image" style={{ backgroundImage: `url(${source})` }}>
                 <a className="subtitle" href={source} target="_blank">© Pixabay</a>
+                <FontAwesomeIcon className="bookmark-icon" onClick={toggleIcon} icon={bookmarkIcon} />
             </div>
-            <FontAwesomeIcon onClick={toggleIcon} icon={bookmarkIcon} />
         </div>
     )
 }
