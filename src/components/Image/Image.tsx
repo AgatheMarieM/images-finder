1bc54112a375b9e02282897b0ace0004ac28cd74
agathe marie <agathe.maillol@gmail.com>
2023-03-27 14:59:20 +0100

    imported heart icons (regular+solid) from FontAwesome library

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,5 +1,10 @@
 import "./Image.css";
 import { ImageProps } from "../../Interface";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faSolidHeart} from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
+
+
 
 
 export default function Image({ source }: ImageProps) {
@@ -8,7 +13,11 @@ export default function Image({ source }: ImageProps) {
         <div className="container">
             <div className="image" data-testid="image" style={{ backgroundImage: `url(${source})` }}>
                 <a className="subtitle" href={source} target="_blank">© Pixabay</a>
-            </div>
+            </div>           
+        
+            <FontAwesomeIcon icon={faRegularHeart} />
+            <FontAwesomeIcon icon={faSolidHeart} />
+        
         </div>
     )
 }
