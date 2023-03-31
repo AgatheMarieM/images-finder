79a4737babe82fd7387cb6f35ec790b55cfb2d6a
agathe marie <agathe.maillol@gmail.com>
2023-03-31 17:55:55 +0100

    imported FavoritesContext to manage favorites state through components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import './App.css';
 import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 import FinderPage from './pages/FinderPage/FinderPage';
-import { FC } from "react";
+import { useState, FC } from "react";
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 import Header from './components/Header/Header';
-
+import FavoritesContext from './context/favorites-context';
 
 const App: FC = () => {
+  //const [favorites, setFavorites] = useState<string[]>([]);
+
   return (
-    <BrowserRouter>
-      <nav>
-        <Link className='image-finder' to='/'>
-          <Header />
-        </Link>
-        <NavLink to='/'>Home </NavLink>
-        <NavLink to='/favorites'>Favorites</NavLink>
-      </nav>
-      <Routes>
-        <Route path='/' element={<FinderPage />} />
-        <Route path='/favorites' element={<FavoritesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <FavoritesContext.Provider value={{
+      favorites: ['favorite array test']
+    }}>
+      <BrowserRouter>
+        <nav>
+          <Link className='image-finder' to='/'>
+            <Header />
+          </Link>
+          <NavLink to='/'>Home </NavLink>
+          <NavLink to='/favorites'>Favorites</NavLink>
+        </nav>
+        <Routes>
+          <Route path='/' element={<FinderPage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </FavoritesContext.Provider>
+
   )
 }
 
