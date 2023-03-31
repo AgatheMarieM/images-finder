5f04172443a9855a364f4dfcf99c9bbadff7a35f
agathe marie <agathe.maillol@gmail.com>
2023-03-31 15:11:20 +0100

    -toggleIcon adding image source to favorites array -each div image contained in a card component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,28 +1,32 @@
 import "./Image.css";
-import { useState } from "react";
+import Card from "../Card/Card";
+import { useState, useEffect, FC } from "react";
 import { ImageProps } from "../../Interface";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookmark as faBookmarkSolid } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons';
 
-
-
-export default function Image({ source }: ImageProps) {
-
-    const [bookmarkIcon, setHeartIcon] = useState(faBookmarkRegular);
-    const [toggle, setToggle] = useState(true);
+const Image = ({ source, favorites, setFavorites }: ImageProps) => {
+    const [favorite, setFavorite] = useState(false);
 
     function toggleIcon() {
-        setToggle(!toggle);
-        setHeartIcon(bookmarkIcon === faBookmarkRegular ? faBookmarkSolid : faBookmarkRegular);
+        if (favorite) {
+            setFavorites(favorites.filter((fav) => fav !== source));
+            setFavorite(false);
+        } else {
+            setFavorites([...favorites, source]);
+            setFavorite(true);
+        }
     }
 
     return (
-        <div className="container">
-            <div className="image" data-testid="image" style={{ backgroundImage: `url(${source})` }}>
-                <a className="subtitle" href={source} target="_blank">© Pixabay</a>
-                <FontAwesomeIcon className="bookmark-icon" onClick={toggleIcon} icon={bookmarkIcon} />
-            </div>
-        </div>
+        <Card className="image-container">        
+                <div className="image" data-testid="image" style={{ backgroundImage: `url(${source})` }}>
+                    <a className="subtitle" href={source} target="_blank">© Pixabay</a>
+                    <FontAwesomeIcon className="bookmark-icon" onClick={toggleIcon} icon={favorite ? faBookmarkSolid : faBookmarkRegular} />
+                </div>          
+        </Card>
     )
 }
+
+export default Image
