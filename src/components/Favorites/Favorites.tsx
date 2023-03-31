454164f823c98c78b7cae430ac06ed10461538aa
agathe marie <agathe.maillol@gmail.com>
2023-03-31 15:14:58 +0100

    imported FavoritesProps from Interfaces file

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,13 +1,16 @@
 import './Favorites.css';
 import Pager from '../Pager/Pager';
+import { FavoritesProps } from "../../Interface";
 
 
-const Favorites = () => {
+
+const Favorites = ({favorites}: FavoritesProps) => {
     return (
         <div className='Favorites'>
             <h2>
                 Favorite images
             </h2>
+            <p>looping through favorites array</p>
             <Pager />
         </div>
 
