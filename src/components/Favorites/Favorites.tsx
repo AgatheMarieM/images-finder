132795fd0a46675eaad859f918fce2d8fecc555f
agathe marie <agathe.maillol@gmail.com>
2023-03-31 18:00:36 +0100

    imported useContext from React and implemented FavoritesContext

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,19 +1,23 @@
 import './Favorites.css';
 import Pager from '../Pager/Pager';
 import { FavoritesProps } from "../../Interface";
+import FavoritesContext from '../../context/favorites-context';
+import { useContext } from 'react';
 
 
 
-const Favorites = ({favorites}: FavoritesProps) => {
+const Favorites = ({ favorites }: FavoritesProps) => {
+    const ctx = useContext(FavoritesContext);
     return (
-        <div className='Favorites'>
-            <h2>
-                Favorite images
-            </h2>
-            <p>looping through favorites array</p>
-            <Pager />
-        </div>
-
+   
+            <div className='Favorites'>
+                <h2>
+                    Favorite images
+                </h2>
+                <p>{ctx.favorites}</p>
+                <Pager />
+            </div>
+       
     )
 }
 
