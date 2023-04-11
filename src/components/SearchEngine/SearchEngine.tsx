c14aaccb9e0da59d835f44a066527364705b3fd8
agathe marie <agathe.maillol@gmail.com>
2023-04-11 14:59:30 +0100

    refactored handleSubmit() with ternary operator and also checking whether query.trim().length > 0 to execute the api call or not

--- src/components/SearchEngine/SearchEngine.tsx
+++ src/components/SearchEngine/SearchEngine.tsx
@@ -5,14 +5,14 @@ import { SearchEngineProps } from "../../Interface";
 
 
 const SearchEngine = ({ apiUrl, results, setResults, query, setQuery }: SearchEngineProps) => {
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState('');
     const [totalHits, setTotalHits] = useState();
 
     function updateMessage() {
         if (totalHits === 0) {
             setMessage(`No results could be found`)
-        } else if (results.length>0) {
-            setMessage(`${totalHits} Results for '${query}'`)
+        } else if (results.length > 0) {
+            setMessage(`${totalHits} Results for '${query}'`);
         } else {
             setMessage(``)
         }
@@ -23,23 +23,19 @@ const SearchEngine = ({ apiUrl, results, setResults, query, setQuery }: SearchEn
     }, [results, totalHits]); //update message whenever results or totalHits change  
 
     function handleResponse(response: AxiosResponse) {
-        
         setResults(response.data.hits);
         setTotalHits(response.data.total);
+
     }
 
     function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
-        event.preventDefault();
+        event.preventDefault();//prevent from reloading the page
         setQuery(event.target.value);
     }
 
     function handleSubmit(event: React.MouseEvent<HTMLInputElement>) {
         event.preventDefault();
-        if(query){
-                    axios.get(apiUrl).then(handleResponse);
-        } else {
-            setMessage(`Please type a query`)
-        }
+        query.trim().length > 0 ? axios.get(apiUrl).then(handleResponse) : setMessage(`Please type a query`);
     }
 
     return (
@@ -49,6 +45,7 @@ const SearchEngine = ({ apiUrl, results, setResults, query, setQuery }: SearchEn
                 <input type="submit" value="go!" onClick={handleSubmit} data-testid="go-button" />
             </form>
             <h2 data-testid="message">{message}</h2>
+
         </div>
 
     )
