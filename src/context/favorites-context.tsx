4e3ab1ec3bea9bdcab1d894822e8c6cd7bbebee7
agathe marie <agathe.maillol@gmail.com>
2023-04-11 14:30:54 +0100

    updated favorites-context: - interface IFavorite - interface IFavoritesContext - FavoritesContext contains an object - FavoritesContextProvider defined - exporting both provider and custom useFavoritesContext() hook

--- src/context/favorites-context.tsx
+++ src/context/favorites-context.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, FC, ReactNode, useContext, useEffect } from 'react';
 
-const FavoritesContext = React.createContext({
-    favorites: ['']
-});
+export interface IFavorite {
+    id: number;
+    source: string;
+}
 
-export default FavoritesContext;
+interface IFavoritesContext {
+    favorites: IFavorite[];
+    setFavorites: (favorites: any) => void;
+}
+
+//Context is an empty object
+export const FavoritesContext = React.createContext<IFavoritesContext>({} as IFavoritesContext);
+
+
+//context provider component,
+// takes a child component as a prop named 'children' of type ReactNode.
+export const FavoritesContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites') || '[]'));
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }, [favorites]);
+    //localStorage.setItem(): object updated each time favorites change!
+    //when an image is erased from the favorites as well
+
+    return <FavoritesContext.Provider value={{
+        favorites: favorites,
+        setFavorites: setFavorites,
+    }}>
+        {children}
+    </FavoritesContext.Provider>
+}
+
+//creating custom hook useFavoritesContext()
+export const useFavoritesContext = () => {
+    const favoritesContext = useContext(FavoritesContext);
+    return favoritesContext;
+}
+
+//we export the provider that will wrap around the root, and the hook that we'll use when needed
+export default { Provider: FavoritesContextProvider, useFavoritesContext };
