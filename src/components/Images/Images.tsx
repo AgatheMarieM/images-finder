c487b99e424f7220f5ae1088601769e1e67d5a00
agathe marie <agathe.maillol@gmail.com>
2023-04-11 14:53:45 +0100

    the key should be the id instead of the index, as it should be unique - erased deprecated props are we now have a FavoritesContext, no longer passing favorites and setFavorites to children

--- src/components/Images/Images.tsx
+++ src/components/Images/Images.tsx
@@ -1,14 +1,17 @@
 import Image from "../Image/Image";
 import "./Images.css";
+
 import { ImagesProps } from "../../Interface";
 
-const Images = ({ results, favorites, setFavorites }: ImagesProps) => {
-
+const Images = ({ results }: ImagesProps) => {
     return (
         <div className="images" data-testid="images-box" >
-            {results.map((result, index) => {
+            {results.map((result) => {
                 return (
-                    <Image id={result.id} key={index} source={result.webformatURL} favorites={favorites} setFavorites={setFavorites} />
+                    <Image
+                        id={result.id} key={result.id}
+                        source={result.webformatURL}
+                    />
                 )
             })}
         </div>
