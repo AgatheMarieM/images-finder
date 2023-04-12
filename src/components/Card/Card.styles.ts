717dc5c34fba94bd6daf4802241f20c0d551b973
agathe marie <agathe.maillol@gmail.com>
2023-04-12 15:09:27 +0100

    Created Card.styles.ts

---
+++ src/components/Card/Card.styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 30%;
+    margin: 10px;
+    border-radius: 6%;
+    box-shadow: 2px 2px rgba(128, 128, 128, 0.400);
+`;
