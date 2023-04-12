f4e95de3cc6b6661d9ae7e357d58cf7782f66516
agathe marie <agathe.maillol@gmail.com>
2023-04-12 15:07:59 +0100

    Created Favorites.styles.ts to manage Favorites styles

---
+++ src/components/Favorites/Favorites.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const FavoritesWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+text-align: center;
+    
+`;
+
+export const FavoritesTitle = styled.h2`
+`;
+
+export const FavoritesImages = styled.div`
+margin: 0;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: flex-start;
+`;
+
