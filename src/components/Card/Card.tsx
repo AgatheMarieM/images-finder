4d9c481ad4f40d9d39dde0ad5411e5d302a3ce1f
agathe marie <agathe.maillol@gmail.com>
2023-04-12 15:10:10 +0100

    updated Card.tsx as a styled component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,9 @@
-import "./Card.css";
+import {Wrapper} from './Card.styles';
 
 const Card = (props) => {
     const classes= 'card '+ props.className;
 
-    return <div className={classes}>{props.children}</div>;
+    return <Wrapper className={classes}>{props.children}</Wrapper>;
 }
 
 export default Card
